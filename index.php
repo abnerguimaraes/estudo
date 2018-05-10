4596f837003ae2db01c102516826afcadd3f13a9
abnerguimaraes <abnerguimaraes1983@gmail.com>
2018-05-09 22:01:34 -0300

    teste

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Meu Primeiro PHP</title>
+</head>
+<body>
+	<?php 
+		$nome_usuario = "ABNER";
+		$var1 = 2;
+		$var2 = 2;
+		define (C1, "THIAGO");
+		echo $nome_usuario . C1 . "Teste php";
+
+		if ($var1 == 1){
+
+			echo "Primerio IF";
+
+		} else if($var1 == 2){
+
+			echo "Diferente";
+
+		}
+
+	?>
+
+	<div>teste</div>
+
+</body>
+</html>
